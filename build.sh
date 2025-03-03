3872bdb2ff3adaf05594c5ca7e08a89e6990f847
Xenia <shlykov_misha_2008@mail.ru>
2025-03-03 15:42:44 +0300

    fixed build system

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ DATETIME=$(date '+%Y-%m-%d %H:%M:%S' --utc | tr -d '\n')
 rm -rf $BUILD/*
 mkdir -p $BUILD
 
-cp -r static $BUILD
+cp -r static/* $BUILD
 
 m4 "-DDATETIME=$DATETIME" -DLANG=FI src/std.m4 src/index.html > $BUILD/index.html
 m4 "-DDATETIME=$DATETIME" -DLANG=RU src/std.m4 src/index.html > $BUILD/index.ru.html
